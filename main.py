6128c4e07798c445b7a8c4bc51df30239bb7a2ba
popcat-floppa <zbaac438@gmail.com>
2026-01-07 21:39:25 +1100

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import turtle
+
+colours = ["purple", "cyan", "green"]
+t = turtle.Turtle()
+t.speed (0.2)
+
+for index in range(250):
+    i = int(index % 3)
+    print(index)
+    t.color(colours[i])
+    t.forward(10+index)
+    t.rt(60)
+
+turtle.done()
+            
+               
