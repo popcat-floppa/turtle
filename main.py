67413b7795a79305cdd3e765698f697f8656c111
popcat-floppa <zbaac438@gmail.com>
2026-01-09 21:16:00 +1100

    added triangular pyramid

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ colours = ["purple", "cyan", "green"]
 t = turtle.Turtle()
 t.speed (0.2)
 
-for index in range(250):
+for index in range(300):
     i = int(index % 3)
     print(index)
     t.color(colours[i])
